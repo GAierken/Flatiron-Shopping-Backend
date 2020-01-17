0d30210931a410bc55d808b481c33a2018cb5e74
GAierken <g.aierken@gmail.com>
2020-01-17 16:09:44 -0500

    validations for users added

--- backend_flatiron_shopping/app/controllers/users_controller.rb
+++ backend_flatiron_shopping/app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all 
+        render json: users
+    end 
+    
+    def create
+        
+    end 
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password_digest, :email)
+    end 
 end
