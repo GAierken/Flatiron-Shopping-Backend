c99513a63f01181bf2019c4fb4965cc124a79d3e
GAierken <g.aierken@gmail.com>
2020-01-17 16:58:34 -0500

    controllers and serializers for all models modified

--- backend_flatiron_shopping/app/controllers/orders_controller.rb
+++ backend_flatiron_shopping/app/controllers/orders_controller.rb
@@ -1,3 +1,6 @@
 class OrdersController < ApplicationController
-
+   def index
+      orders = Order.all 
+      render json: orders
+   end 
 end
--- backend_flatiron_shopping/app/controllers/users_controller.rb
+++ backend_flatiron_shopping/app/controllers/users_controller.rb
@@ -5,9 +5,28 @@ class UsersController < ApplicationController
     end 
     
     def create
-        
+        user=User.new(user_params)
+        if user
+            user.save
+        else 
+            flash[:notice] = user.error.messages
+        end 
+
+        render json: user
     end 
 
+    def update
+        user=User.find(params[:id])
+        user.update(user_params)
+
+        render json: user
+    end 
+
+    def destroy
+        user=User.find(params[:id])
+        user.destroy
+        render json: user
+    end 
 
     private
 
