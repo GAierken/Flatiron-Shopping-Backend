67acd34d5e19e10b611ba3fc96dc23830382ff72
GAierken <g.aierken@gmail.com>
2020-01-22 12:33:07 -0500

    jointable created and user could save orders

--- backend_flatiron_shopping/app/controllers/orders_controller.rb
+++ backend_flatiron_shopping/app/controllers/orders_controller.rb
@@ -10,7 +10,14 @@ class OrdersController < ApplicationController
     end 
 
     def create 
+        order = Order.create(order_params)
        
+        render json:order
 
+    end 
+
+    private 
+    def order_params
+        params.require(:order).permit(:user_id, :items)
     end 
 end
